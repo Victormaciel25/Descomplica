75f8cecf2a1418d1a9afc8505f9cc3aad392a959
Victor Maciel <victormaciel.dev@gmail.com>
2023-11-16 01:15:05 -0300

    Update arrays.js

--- CODE/exercicios/arrays.js
+++ CODE/exercicios/arrays.js
@@ -2,6 +2,8 @@ var valores = [8,1,7,2,9]
 
 console.log(valores[3])
 
+console.log("Array invertido: " + valores.reverse());
+
 for (var pos = 0 ; pos < valores.length; pos++){
     console.log("Posicao: " + valores[pos]);
 }
@@ -23,3 +25,23 @@ for (var pos = 0 ; pos < valores.length; pos++){
 var media = soma/valores.length;
 
 console.log(media);
+
+var arr1 = [1,2,3,4,5,6];
+
+console.log(arr1.join('|')); // O join separa o array com o elemento que desejar.
+
+var retirado = arr1.shift(); //  shift retira o primeiro elemento do array
+
+console.log(retirado);
+
+console.log(arr1);
+
+console.log("Posição do numero 3: " + arr1.indexOf(3));
+
+arr1.push(7); // push acrescenta um numero no array
+
+console.log(arr1);
+
+arr1.pop(); // Pop remove o ultimo elemento do array
+
+console.log(arr1);
